f65639b52b4297b25328fa234cb36d3e98ce8a43
ig-or-ka <mish.dubov@gmail.com>
2022-07-09 02:02:25 +0500

    Доработал модель

--- Classes.py
+++ Classes.py
@@ -10,69 +10,92 @@ class indexes:
     node = dict()
 
 class edges: #edges = ребра
-    def __init__(self, edge_type = "sea", edge_id = None, ice_condition = 1, length = 1, incident_nodes = "*id_begin_node*_*id_end_node*", max_throughput = 1, tariff = 1500):
+    # incident_nodes: "*id_begin_node*_*id_end_node*"
+    # tariff: "*tariff_cont*_*tariff_oil*_*tariff_weight*"
+    def __init__(self, edge_type = "sea", edge_id = None, ice_condition = 1, length = 1, incident_nodes = "0_0", max_throughput = 1, tariff = "0_0_0"):
         self.edge_type = edge_type
         self.edge_id = edge_id
         self.ice_condition = ice_condition
         self.length = length
-        self.incident_nodes = incident_nodes
+
+        segs = incident_nodes.split('_')
+        self.id_begin_node = int(segs[0])
+        self.id_end_node = int(segs[1])
+
         self.max_throughput = max_throughput
-        self.tariff = tariff
+
+        segs = tariff.split('_')
+        self.tariff_cont = int(segs[0])
+        self.tariff_oil = int(segs[1])
+        self.tariff_weight = int(segs[2])
+        
         if edge_id != None:
             indexes.edges[edge_id] = self
     def create(self):
         #вытаскиваем поля из таблицы с помощью select и increment counter
         with sq.connect("Ships_Icebreakers.db") as con:
             cur = con.cursor()
-            cur.execute(f"insert into edges (edge_type, edge_id, ice_condition, length, incident_nodes, max_throughput, tariff) values ('{edge_type}', '{edge_id}', '{ice_condition}', '{length}', '{incident_nodes}', '{max_throughput}', '{tariff}')")
-            cur.execute("""SELECT `AUTO_INCREMENT`
-                            FROM  INFORMATION_SCHEMA.TABLES
-                            WHERE TABLE_SCHEMA = 'Ships_Icebreakers.db'
-                            AND   TABLE_NAME   = 'edges';""")
-            result = cur.fetchall()
-            self.edge_id = result[0][0]
-            indexes.edges[result[0][0]] = self
+            cur.execute(f"insert into edges (edge_type, ice_condition, length, incident_nodes, max_throughput, tariff) values ('{self.edge_type}','{self.ice_condition}', '{self.length}', '{self.id_begin_node}_{self.id_end_node}', '{self.max_throughput}', '{self.tariff_cont}_{self.tariff_oil}_{self.tariff_weight}')")
+            cur.execute("select count(*) from edges")      
+            result = cur.fetchone()
+            self.edge_id = result[0]
+            indexes.edges[result[0]] = self
     def update(self):
         #обновление поля из таблицы
         with sq.connect("Ships_Icebreakers.db") as con:
             cur = con.cursor()
             cur.execute(f"""UPDATE edges
-            SET edge_id = {self.edge_id}, ice_condition = {self.ice_condition}, length = {self.length}, incident_nodes = {self.incident_nodes}, max_throughput = {self.incident_nodes}, tariff = {self.tariff};
+                        SET
+                        edge_type = '{self.edge_type}',
+                        ice_condition = {self.ice_condition},
+                        length = {self.length},
+                        incident_nodes = '{self.id_begin_node}_{self.id_end_node}',
+                        max_throughput = {self.max_throughput},
+                        tariff = '{self.tariff_cont}_{self.tariff_oil}_{self.tariff_weight}'
+                        WHERE edge_id = {self.edge_id}
             """)
 
 class ship: #ship = корабль
-    def __init__(self, ship_id = None, edge_position = 1, edge_id = -1, port_id = 1, in_port = True, icebreaker_id = 1, max_capacity = 1, node_id = 1, coordinates = 0, cargo_type = "", caravan_condition = True):
+    def __init__(self, ship_id = None, edge_position = 1, edge_id = -1, port_id = 1, in_port = True, icebreaker_id = 1, max_capacity = 1, node_id = 1, coordinates = 0, cargo_type = "", caravan_condition = False):
         self.ship_id = ship_id
         self.edge_position = edge_position
         self.edge_id = edge_id
         self.port_id = port_id
-        self.in_port = in_port
+        self.in_port = bool(in_port)
         self.icebreaker_id = icebreaker_id
         self.max_capacity = max_capacity
         self.node_id = node_id
         self.coordinates = coordinates
         self.cargo_type = cargo_type
-        self.caravan_condition = caravan_condition
+        self.caravan_condition = bool(caravan_condition)
         if ship_id != None:
             indexes.ship[ship_id] = self
     def create(self):
         #вытаскиваем поля из таблицы с помощью select и increment counter
         with sq.connect("Ships_Icebreakers.db") as con:
             cur = con.cursor()
-            cur.execute(f"insert into ship (ship_id, edge_position, edge_id, port_id, in_port, icebreaker_id, max_capacity, node_id, coordinates, cargo_type, caravan_condition) values ('{ship_id}', '{edge_position}', '{edge_id}', '{port_id}', '{in_port}', '{icebreaker_id}', '{max_capacity}, '{node_id}', '{coordinates}', '{cargo_type}', '{caravan_condition}')")
-            cur.execute("""SELECT `AUTO_INCREMENT`
-                            FROM  INFORMATION_SCHEMA.TABLES
-                            WHERE TABLE_SCHEMA = 'Ships_Icebreakers.db'
-                            AND   TABLE_NAME   = 'ship';""")
-            result = cur.fetchall()
-            self.ship_id = result[0][0]
-            indexes.ship[result[0][0]] = self
+            cur.execute(f"insert into ship (edge_position, edge_id, port_id, in_port, icebreaker_id, max_capacity, node_id, coordinates, cargo_type, caravan_condition) values ('{self.edge_position}', '{self.edge_id}', '{self.port_id}', '{int(self.in_port)}', '{self.icebreaker_id}', '{self.max_capacity}', '{self.node_id}', '{self.coordinates}', '{self.cargo_type}', '{int(self.caravan_condition)}')")
+            cur.execute("select count(*) from ship")      
+            result = cur.fetchone()
+            self.ship_id = result[0]
+            indexes.ship[result[0]] = self
     def update(self):
         #обновление поля из таблицы
         with sq.connect("Ships_Icebreakers.db") as con:
             cur = con.cursor()
             cur.execute(f"""UPDATE ship
-            SET ship_id = {self.ship_id}, edge_position = {self.edge_position}, edge_id = {self.edge_id}, port_id = {self.port_id}, in_port = {self.in_port}, icebreaker_id = {self.icebreaker_id}, max_capacity = {self.max_capacity}, node_id = {self.node_id}, coordinates = {self.coordinates}, cargo_type = {self.cargo_type}, caravan_condition = {self.caravan_condition}
+                        SET
+                        edge_position = {self.edge_position},
+                        edge_id = {self.edge_id},
+                        port_id = {self.port_id},
+                        in_port = {int(self.in_port)},
+                        icebreaker_id = {self.icebreaker_id},
+                        max_capacity = {self.max_capacity},
+                        node_id = {self.node_id},
+                        coordinates = {self.coordinates},
+                        cargo_type = '{self.cargo_type}',
+                        caravan_condition = {int(self.caravan_condition)}
+                        WHERE ship_id = {self.ship_id}
             """)
 
 class consignment: #consignment = партия груза
@@ -80,68 +103,81 @@ class consignment: #consignment = партия груза
         self.cargo_id = cargo_id
         self.size = size
         self.node_destination_id = node_destination_id
-        self.ship_immediately = ship_immediately
+        self.ship_immediately = bool(ship_immediately)
         self.type_refer = type_refer
         self.id_refer = id_refer
         self.coordinates = coordinates
-        self.contracted = contracted
+        self.contracted = bool(contracted)
         if cargo_id != None:
             indexes.consignment[cargo_id] = self
     def create(self):
         #вытаскиваем поля из таблицы с помощью select и increment counter
         with sq.connect("Ships_Icebreakers.db") as con:
             cur = con.cursor()
-            cur.execute(f"insert into consignment (cargo_id, size, node_destination_id, ship_immediately, type_refer, id_refer, coordinates, contracted) values ('{cargo_id}', '{size}', '{node_destination_id}', '{ship_immediately}', '{type_refer}', '{id_refer}', '{coordinates}', '{contracted}')")
-            cur.execute("""SELECT `AUTO_INCREMENT`
-                            FROM  INFORMATION_SCHEMA.TABLES
-                            WHERE TABLE_SCHEMA = 'Ships_Icebreakers.db'
-                            AND   TABLE_NAME   = 'consignment';""")
-            result = cur.fetchall()
-            self.cargo_id = result[0][0]
-            indexes.consignment[result[0][0]] = self
+            cur.execute(f"insert into consignment (size, node_destination_id, ship_immediately, type_refer, id_refer, coordinates, contracted) values ('{self.size}', '{self.node_destination_id}', '{int(self.ship_immediately)}', '{self.type_refer}', '{self.id_refer}', '{self.coordinates}', '{int(self.contracted)}')")
+            cur.execute("select count(*) from consignment")      
+            result = cur.fetchone()
+            self.cargo_id = result[0]
+            indexes.consignment[result[0]] = self
     def update(self):
         #обновление поля из таблицы
         with sq.connect("Ships_Icebreakers.db") as con:
             cur = con.cursor()
             cur.execute(f"""UPDATE consignment
-            SET cargo_id = {self.cargo_id}, size = {self.size}, node_destination_id = {self.node_destination_id}, ship_immediately = {self.ship_immediately}, type_refer = {type_refer}, id_refer = {self.id_refer}, coordinates = {self.coordinates}, contracted = {self.contracted}
+                        SET
+                        size = {self.size},
+                        node_destination_id = {self.node_destination_id},
+                        ship_immediately = {int(self.ship_immediately)},
+                        type_refer = {self.type_refer},
+                        id_refer = {self.id_refer},
+                        coordinates = {self.coordinates},
+                        contracted = {int(self.contracted)}
+                        WHERE cargo_id = {self.cargo_id}
             """)
 
 class icebreaker: #icebreaker = ледокол
     def __init__(self, icebreaker_id = None, edge_position = 0, prepare_caravan = True, edge_id = 0, port_id = 0, node_destination_id = 1, speed = 40,shipsin_caravan = True):
         self.icebreaker_id = icebreaker_id
         self.edge_position = edge_position
-        self.prepare_caravan = prepare_caravan
+        self.prepare_caravan = bool(prepare_caravan)
         self.edge_id = edge_id
         self.port_id = port_id
         self.node_destination_id = node_destination_id
         self.speed = speed
-        self.shipsin_caravan = shipsin_caravan
+        self.shipsin_caravan = bool(shipsin_caravan)
         if icebreaker_id != None:
             indexes.icebreaker[icebreaker_id] = self
     def create(self):
         #вытаскиваем поля из таблицы с помощью select и increment counter
         with sq.connect("Ships_Icebreakers.db") as con:
             cur = con.cursor()
-            cur.execute(f"insert into icebreaker (icebreaker_id, edge_position, prepare_caravan, edge_id, port_id, node_destination_id, speed, shipsin_caravan) values ('{icebreaker_id}', '{edge_position}', '{prepare_caravan}', '{edge_id}', '{node_destination_id}', '{speed}', '{shipsin_caravan}')")
-            cur.execute("""SELECT `AUTO_INCREMENT`
-                            FROM  INFORMATION_SCHEMA.TABLES
-                            WHERE TABLE_SCHEMA = 'Ships_Icebreakers.db'
-                            AND   TABLE_NAME   = 'icebreaker';""")
-            result = cur.fetchall()
-            self.icebreaker_id = result[0][0]
-            indexes.icebreaker[result[0][0]] = self
+            cur.execute(f"insert into icebreaker (edge_position, prepare_caravan, edge_id, port_id, node_destination_id, speed, shipsin_caravan) values ('{self.edge_position}', '{int(self.prepare_caravan)}', '{self.edge_id}', '{self.port_id}', '{self.node_destination_id}', '{self.speed}', '{int(self.shipsin_caravan)}')")
+            cur.execute("select count(*) from icebreaker")      
+            result = cur.fetchone()
+            self.icebreaker_id = result[0]
+            indexes.icebreaker[result[0]] = self
     def update(self):
         #обновление поля из таблицы
         with sq.connect("Ships_Icebreakers.db") as con:
             cur = con.cursor()
             cur.execute(f"""UPDATE icebreaker
-            SET icebreaker_id = {self.icebreaker_id}, edge_position = {self.edge_position}, prepare_caravan = {self.prepare_caravan}, edge_id = {self.edge_id}, port_id = {self.port_id}, node_destination_id = {self.node_destination_id}, speed = {self.speed}, shipsin_caravan = {self.shipsin_caravan}
+                        SET edge_position = {self.edge_position}, 
+                        prepare_caravan = {int(self.prepare_caravan)},
+                        edge_id = {self.edge_id},
+                        port_id = {self.port_id}, 
+                        node_destination_id = {self.node_destination_id}, 
+                        speed = {self.speed}, 
+                        shipsin_caravan = {int(self.shipsin_caravan)}
+                        WHERE icebreaker_id = {self.icebreaker_id}
             """)
 
 class node: #node = узел
-    def __init__(self, coordinates = 0, node_id = None):
-        self.coordinates = coordinates
+    # coordinates: "*X*_*Y*"
+    def __init__(self, node_id = None, coordinates = "0_0"):
+        segs = coordinates.split("_")
+        self.coordinate_X = int(segs[0])
+        self.coordinate_Y = int(segs[1])
+
         self.node_id = node_id
         if node_id != None:
             indexes.node[node_id] = self
@@ -149,40 +185,48 @@ class node: #node = узел
         #вытаскиваем поля из таблицы с помощью select и increment counter
         with sq.connect("Ships_Icebreakers.db") as con:
             cur = con.cursor()
-            cur.execute(f"insert into node (coordinates, node_id) values ('{coordinates}', '{node_id}')")
-            cur.execute("""SELECT `AUTO_INCREMENT`
-                            FROM  INFORMATION_SCHEMA.TABLES
-                            WHERE TABLE_SCHEMA = 'Ships_Icebreakers.db'
-                            AND   TABLE_NAME   = 'node';""")
-            result = cur.fetchall()
-            self.node_id = result[0][0]
-            indexes.node[result[0][0]] = self
+            cur.execute(f"insert into node (coordinates) values ('{self.coordinate_X}_{self.coordinate_Y}')")
+            cur.execute("select count(*) from node")      
+            result = cur.fetchone()
+            self.node_id = result[0]
+            indexes.node[result[0]] = self
     def update(self):
         #обновление поля из таблицы
         with sq.connect("Ships_Icebreakers.db") as con:
             cur = con.cursor()
             cur.execute(f"""UPDATE node
-            SET coordinates = {self.coordinates}, node_id = {self.node_id}, """)
+                SET coordinates = '{self.coordinate_X}_{self.coordinate_Y}'
+                WHERE node_id = {self.node_id}""")
 def full_info():
     with sq.connect("Ships_Icebreakers.db") as con:
         cur = con.cursor()
         cur.execute("SELECT * FROM edges")
+        #print("edges =============")
         result = cur.fetchall()
         for row in result:
+            #print(row)
             edges(*row)
         cur.execute("SELECT * FROM ship")
+        #print("ship =============")
         result = cur.fetchall()
         for row in result:
+            #print(row)
             ship(*row)
         cur.execute("SELECT * FROM consignment")
+        #print("consignment =============")
         result = cur.fetchall()
         for row in result:
+            #print(row)
             consignment(*row)
         cur.execute("SELECT * FROM icebreaker")
+        #print("icebreaker =============")
         result = cur.fetchall()
         for row in result:
+            #print(row)
             icebreaker(*row)
         cur.execute("SELECT * FROM node")
+        #print("node =============")
         result = cur.fetchall()
         for row in result:
+            #print(row)
             node(*row)
--- DB.py
+++ DB.py
@@ -3,10 +3,10 @@ import sqlite3 as sq
 with sq.connect("Ships_Icebreakers.db") as con:
     cur = con.cursor()
 
-    # добавь столбцы
     # id ребра (INT PRIMARY KEY autoincrement)
     # ледовая обстановка, 0 если не СМП (INT)
-    cur.execute("DROP TABLE IF EXISTS edges")
+
+    #cur.execute("DROP TABLE IF EXISTS edges")
     cur.execute("""CREATE TABLE IF NOT EXISTS edges(
         edge_type TEXT,
         edge_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,7 +17,6 @@ with sq.connect("Ships_Icebreakers.db") as con:
         tariff TEXT
     )""")
 
-    # добавь столбцы
     # id корабля (INT PRIMARY KEY autoincrement)
     # положение на ребре (INT)
     # id ребра, если плыпет, id порта, если стоит (INT)
@@ -25,22 +24,22 @@ with sq.connect("Ships_Icebreakers.db") as con:
     # id ледокола, если находится в караване (INT)
     # максимальная вместимость (INT)
     # id узела назначения (INT)
-    cur.execute("DROP TABLE IF EXISTS ship")
+
+    #cur.execute("DROP TABLE IF EXISTS ship")
     cur.execute("""CREATE TABLE IF NOT EXISTS ship(
-    ship_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    edge_position INTEGER,
-    edge_id INTEGER,
-    port_id INTEGER,
-    in_port BOOLEAN,
-    icebreaker_id INTEGER,
-    max_capacity INTEGER,
-    node_id INTEGER,
-    coordinates INTEGER NOT NULL DEFAULT "0.000000, 0.000000",
-    cargo_type TEXT,
-    caravan_condition BOOLEAN
+        ship_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        edge_position INTEGER,
+        edge_id INTEGER,
+        port_id INTEGER,
+        in_port BOOLEAN,
+        icebreaker_id INTEGER,
+        max_capacity INTEGER,
+        node_id INTEGER,
+        coordinates INTEGER,
+        cargo_type TEXT,
+        caravan_condition BOOLEAN
     )""")
 
-    # добавь столбцы
     # id груза (INT PRIMARY KEY autoincrement)
     # объем (INT),
     # узле назвачения (INT id узла, куда необходимо доставить груз),
@@ -48,40 +47,40 @@ with sq.connect("Ships_Icebreakers.db") as con:
     # тип принадлежности (INT 1 - узел, 2 - ребро, 3 - кораблю)
     # id принадлежности (INT id корабля, ребра или узла)
     # coordinates INTEGER, 0 если привязан к кораблю
-    cur.execute("DROP TABLE IF EXISTS consignment")
+
+    #cur.execute("DROP TABLE IF EXISTS consignment")
     cur.execute("""CREATE TABLE IF NOT EXISTS consignment(
-    cargo_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    size INTEGER,
-    node_destination_id INTEGER,
-    ship_immediately BOOLEAN,
-    type_refer INTEGER,
-    id_refer INTEGER,
-    coordinates INTEGER,
-    contracted BOOLEAN
+        cargo_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        size INTEGER,
+        node_destination_id INTEGER,
+        ship_immediately BOOLEAN,
+        type_refer INTEGER,
+        id_refer INTEGER,
+        coordinates INTEGER,
+        contracted BOOLEAN
     )""")
 
-    # добавь столбцы
     # id ледокола (INT PRIMARY KEY autoincrement)
     # положение на ребре (INT)
     # собирает караван (BOOLEAN)
     # id ребра, если плыпет, id порта, если стоит (INT)
     # id узела назначения (INT)
-    cur.execute("DROP TABLE IF EXISTS icebreaker")
+
+    #cur.execute("DROP TABLE IF EXISTS icebreaker")
     cur.execute("""CREATE TABLE IF NOT EXISTS icebreaker(
-    icebreaker_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    edge_position INTEGER,
-    prepare_caravan BOOLEAN,
-    edge_id INTEGER,
-    port_id INTEGER,
-    node_destination_id INTEGER,
-    speed INTEGER,
-    shipsin_caravan BOOLEAN
+        icebreaker_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        edge_position INTEGER,
+        prepare_caravan BOOLEAN,
+        edge_id INTEGER,
+        port_id INTEGER,
+        node_destination_id INTEGER,
+        speed INTEGER,
+        shipsin_caravan BOOLEAN
     )""")
 
-    # добавь столбцы
-    # добавь PRIMARY KEY autoincrement id каждого узла
-    cur.execute("DROP TABLE IF EXISTS node")
+       
+    #cur.execute("DROP TABLE IF EXISTS node")
     cur.execute("""CREATE TABLE IF NOT EXISTS node(
-    node_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    coordinates TEXT
+        node_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        coordinates TEXT
     )""")
